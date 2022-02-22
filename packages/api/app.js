0239cdb33c605c4e9a9b910b080e41553375420e
Recep Omer Zorlu <recepomerzorlu@gmail.com>
2022-02-22 19:38:58 +0100

    Http request for gyro sensor implemented basically

--- packages/api/app.js
+++ packages/api/app.js
@@ -6,6 +6,7 @@ var express = require('express');
 
 var indexRouter = require('./routes/index');
 var accelerometerRouter = require('./routes/accelerometerRoutes');
+var gyroRouter = require('./routes/gyroRouter');
 
 var app = express();
 
@@ -17,6 +18,7 @@ var app = express();
 
 app.use('/', indexRouter);
 app.use('/accel', accelerometerRouter);
+app.use('/gyro', gyroRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
