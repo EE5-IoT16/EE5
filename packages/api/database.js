a14c5fe97d36826a71038585bec62db16ee9ff20
Samuel <samuel.tocci@gmail.com>
2022-02-22 16:33:54 +0100

    Connection to mongoDB cluster

--- packages/api/database.js
+++ packages/api/database.js
@@ -1,11 +1,22 @@
 const {MongoClient} = require('mongodb');
 
+const uri = "mongodb+srv://SamuelTocci:BAKDpGf5aXCuTjqQ@ee5.5nq6q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri)
+
 async function main(){
     /**
     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      */
-    const uri = "mongodb+srv://SamuelTocci:BAKDpGf5aXCuTjqQ@ee5.5nq6q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    const client = new MongoClient(uri)
-
-    
+    try {
+        // Connect the client to the server
+        await client.connect();
+        // Establish and verify connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected successfully to server");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
 }
+
+main();
