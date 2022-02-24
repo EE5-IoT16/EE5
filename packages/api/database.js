62d7508dcc29f3f293ff4f5c056e12ee52bd4c20
Samuel <samuel.tocci@gmail.com>
2022-02-24 10:48:45 +0100

    seperated read and write to db

--- packages/api/database.js
+++ packages/api/database.js
@@ -5,7 +5,7 @@ const client = new MongoClient(uri)
 
 testDoc = {type : "heartrate", bpm : 90}
 
-async function insertOneDb(doc){
+async function insertOne(doc){
     /**
     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      */
@@ -20,14 +20,7 @@ async function insertOneDb(doc){
         const database = client.db('myFirstDatabase');
         const hrCollection = database.collection('heartrate');
 
-        //const res = hrCollection.insertOne(doc);
-
-        const findResult = await hrCollection.find({type: "heartrate"})
-        await findResult.forEach(console.dir)
-
-        // const query = { type: 'heartrate' };
-        // const movie = await hrCollection.findOne(query);
-        // console.log(movie);
+        const res = hrCollection.insertOne(doc);
 
       } finally {
         // Ensures that the client will close when you finish/error
@@ -35,4 +28,25 @@ async function insertOneDb(doc){
       }
 }
 
-insertOneDb(testDoc);
+async function findAll(findQuery){
+  try {
+    // Connect the client to the server
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+
+    const database = client.db('myFirstDatabase');
+    const hrCollection = database.collection('heartrate');
+
+
+    const findResult = await hrCollection.find(findQuery);
+    await findResult.forEach(console.dir);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+//insertOne(testDoc);
+findAll({type: "heartrate"});
