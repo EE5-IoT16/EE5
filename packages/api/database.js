9dde3f54effe41035741fd457c10095424da8bd0
Samuel <samuel.tocci@gmail.com>
2022-02-24 09:39:46 +0100

    Merge branch 'main' of https://github.com/SamuelTocci/EE5 into main

--- packages/api/database.js
+++ packages/api/database.js
@@ -19,4 +19,17 @@ async function main(){
       }
 }
 
+async function insertOne(){
+  
+}
+
+// db.inventory.insertOne(
+//   { "item" : "canvas",
+//     "qty" : 100,
+//     "tags" : ["cotton"],
+//     "size" : { "h" : 28, "w" : 35.5, "uom" : "cm" }
+//   }
+// )
+
+
 main();
