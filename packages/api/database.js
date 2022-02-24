4ddff37cb39501637e968356a186d56a3b3ac0e6
Samuel <samuel.tocci@gmail.com>
2022-02-24 10:20:17 +0100

    succesfully inserted and read doc on database

--- packages/api/database.js
+++ packages/api/database.js
@@ -3,6 +3,8 @@ const {MongoClient} = require('mongodb');
 const uri = "mongodb+srv://SamuelTocci:BAKDpGf5aXCuTjqQ@ee5.5nq6q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri)
 
+testDoc = {type : "heartrate", bpm : 86}
+
 async function main(){
     /**
     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
@@ -13,14 +15,28 @@ async function main(){
         // Establish and verify connection
         await client.db("admin").command({ ping: 1 });
         console.log("Connected successfully to server");
+
+
+        const database = client.db('myFirstDatabase');
+        const movies = database.collection('heartrate');
+
+        const res = movies.insertOne(testDoc);
+
+        const query = { type: 'heartrate' };
+        const movie = await movies.findOne(query);
+        console.log(movie);
+
       } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
       }
 }
 
-async function insertOne(){
-  
+
+async function insertOneDoc(){
+  const res = await collection.insertOne(testDoc);
+  console.log(res)
+  return res
 }
 
 // db.inventory.insertOne(
