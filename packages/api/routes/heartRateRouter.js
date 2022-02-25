49f19e8572de362e0f0944ed3ed445906792ca65
Samuel <samuel.tocci@gmail.com>
2022-02-25 15:46:40 +0100

    Heartrate get and post database integration done

--- packages/api/routes/heartRateRouter.js
+++ packages/api/routes/heartRateRouter.js
@@ -4,11 +4,14 @@ var db = require("../database")
 
 router.get('/', function(req, res, next) {
   res.send('Heart rate get method called.');
-  db.findAll({type: "heartrate"});
+  db.findAll({type: "heartrate"}); //TODO: limit returned data
 });
 
 router.post('/', function(req, res, next) {
     res.send('Heart rate post method called.');
+    const doc = req.query;
+    db.insertOne(doc);
+    db.findAll({type: "heartrate"});
 });
 
 router.put('/', function(req, res, next) {
